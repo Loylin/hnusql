44a6f280b66398f3ef988fb8543fbcbe40a0377f
Loylin <935020188@qq.com>
2017-07-06 00:11:34 +0800

    更改配置路径

--- hnusql/mysql.cc
+++ hnusql/mysql.cc
@@ -1,33 +1,9 @@
 /*
-   Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; version 2 of the License.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
-*/
-
-/* mysql command tool
- * Commands compatible with mSQL by David J. Hughes
- *
- * Written by:
- *   Michael 'Monty' Widenius
- *   Andi Gutmans  <andi@zend.com>
- *   Zeev Suraski  <zeev@zend.com>
- *   Jani Tolonen  <jani@mysql.com>
- *   Matt Wagner   <matt@mysql.com>
- *   Jeremy Cole   <jcole@mysql.com>
- *   Tonu Samuel   <tonu@mysql.com>
- *   Harrison Fisk <harrison@mysql.com>
- *
+	HnuSQL客户端主程序
+	李帅  罗玉林  邓磊鑫
+	2017.6.26
+	湖南大学信息科学与工程学院
+	暑假小学期数据库项目第二组
  **/
 
 #include "client_priv.h"
